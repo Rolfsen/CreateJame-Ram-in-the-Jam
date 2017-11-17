a4b0d85e6444e63d6e905bde1b5f5683a5b04462
Rolfsen <rasmus.rolfsen@mail.dk>
2017-11-17 22:54:15 +0100

    Added some keyinputs to controls

--- New Unity Project 5/Assets/Scripts/PlayerControl.cs
+++ New Unity Project 5/Assets/Scripts/PlayerControl.cs
@@ -3,14 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour {
+	[Header("Increase Ram Force")]
+	[SerializeField] private KeyCode powerUpButton0;
+	[SerializeField] private KeyCode powerUpButton1;
+	[SerializeField] private KeyCode powerUpButton2;
+	[SerializeField] private KeyCode powerUpButton3;
+	[Header("Attack Other Ram")]
+	[SerializeField] private KeyCode attackButton0;
+	[SerializeField] private KeyCode attackButton1;
+	[SerializeField] private KeyCode attackButton2;
+	[SerializeField] private KeyCode attackpButton3;
+	[Header("Dodge attacks")]
+	[SerializeField] private KeyCode dogdeButton0;
+	[SerializeField] private KeyCode dogdeButton1;
+	[SerializeField] private KeyCode dogdeButton2;
+	[SerializeField] private KeyCode dogdepButton3;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+
+
+
 	// Update is called once per frame
 	void Update () {
-		
+
+		if (Input.GetKeyDown(powerUpButton1))
+		{
+			Debug.Log("PowerUp Button1 pressed by: " + gameObject.name);
+		}
+		if (Input.GetKeyDown(powerUpButton2))
+		{
+			Debug.Log("PowerUp Button2 pressed by: " + gameObject.name);
+		}
+		if (Input.GetKeyDown(powerUpButton3))
+		{
+			Debug.Log("PowerUp Button3 pressed by: " + gameObject.name);
+		}
+		if (Input.GetKeyDown(powerUpButton0))
+		{
+			Debug.Log("PowerUp Button0 pressed by: " + gameObject.name);
+		}
 	}
 }
