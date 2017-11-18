d4426602d7640cc7c41045ab96d25a3618dbb5be
Rolfsen <rasmus.rolfsen@mail.dk>
2017-11-18 18:12:32 +0100

    Event Manager

---
+++ New Unity Project 5/Assets/Scripts/Events/EventExampleListener.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class EventExampleListener : MonoBehaviour
+{
+
+	// Remove event when object is destroyed
+	private void OnDestroy()
+	{
+		EventManager.StopListening("DebugEvent", DebugEvent);
+	}
+
+	// Enable the event
+	private void OnEnable()
+	{
+		EventManager.StartListening("DebugEvent", DebugEvent);
+	}
+
+	// Disable the event
+	private void OnDisable()
+	{
+		EventManager.StopListening("DebugEvent", DebugEvent);
+	}
+
+	// Function To Be Executed
+	private void DebugEvent(object e)
+	{
+		Debug.Log(e);
+	}
+}
---
+++ New Unity Project 5/Assets/Scripts/Events/EventManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+
+[System.Serializable]
+public class EventsArgs : UnityEvent<object>
+{
+
+}
+
+
+public class EventManager : MonoBehaviour
+{
+
+	private Dictionary<string, EventsArgs> eventDictionary;
+	private static EventManager eventManagerObject;
+
+	public static EventManager instance
+	{
+		get
+		{
+			if (!eventManagerObject)
+			{
+				eventManagerObject = FindObjectOfType(typeof(EventManager)) as EventManager;
+
+				if (!eventManagerObject)
+				{
+					Debug.LogError("Put an active Event Manager into the scene");
+				}
+				else
+				{
+					eventManagerObject.Init();
+				}
+			}
+			return eventManagerObject;
+		}
+	}
+
+	void Init()
+	{
+		if (eventDictionary == null)
+		{
+			eventDictionary = new Dictionary<string, EventsArgs>();
+		}
+	}
+
+	public static void StartListening(string eventName, UnityAction<object> listener)
+	{
+		EventsArgs thisEvent = null;
+		if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+		{
+			thisEvent.AddListener(listener);
+		}
+		else
+		{
+			thisEvent = new EventsArgs();
+			thisEvent.AddListener(listener);
+			instance.eventDictionary.Add(eventName, thisEvent);
+		}
+	}
+
+	public static void StopListening(string eventName, UnityAction<object> listener)
+	{
+		if (eventManagerObject == null) return;
+
+		EventsArgs thisEvent = null;
+		if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+		{
+			thisEvent.RemoveListener(listener);
+		}
+	}
+
+	public static void RemoveAllListerners()
+	{
+		instance.eventDictionary.Clear();
+	}
+
+	public static void TriggerEvent(string eventName, object e)
+	{
+		EventsArgs thisEvent = null;
+		if (instance.eventDictionary.TryGetValue(eventName, out thisEvent))
+		{
+			thisEvent.Invoke(e);
+		}
+	}
+}
