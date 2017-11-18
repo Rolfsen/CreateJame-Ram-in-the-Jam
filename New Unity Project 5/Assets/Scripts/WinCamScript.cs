46a00f85a42bc68c4205253fbbdab9f821e17f0d
Samuel Howell <rugnirtheviking@gmail.com>
2017-11-18 21:33:54 +0100

    Finale Cam

---
+++ New Unity Project 5/Assets/Scripts/WinCamScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinCamScript : MonoBehaviour {
+    [SerializeField]
+    public Transform lookTarget;
+    [SerializeField]
+    public Transform followTarget;
+
+    private Vector3 positionOffset;
+    // Use this for initialization
+    void Start () {
+        positionOffset = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(lookTarget);
+        transform.position = followTarget.transform.position + positionOffset;
+    }
+}
