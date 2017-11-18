3dd3b9e5bd000621588ec084cab7b5295c055407
Samuel Howell <rugnirtheviking@gmail.com>
2017-11-19 00:17:29 +0100

    wrong key penalty

--- New Unity Project 5/Assets/Scripts/PlayerControl.cs
+++ New Unity Project 5/Assets/Scripts/PlayerControl.cs
@@ -225,6 +225,9 @@ public class PlayerControl : MonoBehaviour
     private void WrongKeyPress(KeyCode ButtonPressed)
     {
 		error.Play();
+        comboJuice = 0;
+        jumpForce -= increaseForce;
+
 		// Bad things happen
     }
 
