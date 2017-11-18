cdf7330f39ece9545dd81f31a8469e74e4b001b9
Rolfsen <rasmus.rolfsen@mail.dk>
2017-11-18 21:15:36 +0100

    Sounds in menu :>

--- New Unity Project 5/Assets/Scripts/UI/SliderUI.cs
+++ New Unity Project 5/Assets/Scripts/UI/SliderUI.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
+
 
 public class SliderUI : MonoBehaviour
 {
-
+	public AudioMixer audio;
 	private Slider slider;
 
 	private void Start()
 	{
 		slider = GetComponent<Slider>();
 	}
-
+/*
 	private void OnEnable()
 	{
 		slider = GetComponent<Slider>();
@@ -24,12 +26,11 @@ public class SliderUI : MonoBehaviour
 		{
 			Debug.LogError("Slider is: " + slider);
 		}
-	}
+	}*/
 
 
 	public void UpdateSoundVolume()
 	{
 		PlayerPrefs.SetFloat("SoundVolume", slider.value);
-		FindObjectOfType<SoundPlayer>().VolumeChanged();
 	}
 }
