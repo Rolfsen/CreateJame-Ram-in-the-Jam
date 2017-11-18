983aa3fd33b4e6c33f4ecf05d397cefd2f270152
Unknown <rasmus.rolfsen@mail.dk>
2017-11-18 10:38:07 +0100

    Made Temp Main Menu

---
+++ New Unity Project 5/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager: MonoBehaviour {
+
+	[SerializeField]
+	string sceneName;
+	[SerializeField]
+	GameObject mainMenu;
+
+	public void GoToScene ()
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+	public void QuitGame ()
+	{
+		Application.Quit();
+	}
+	public void Settings ()
+	{
+		mainMenu.SetActive(false);		
+	}
+}
