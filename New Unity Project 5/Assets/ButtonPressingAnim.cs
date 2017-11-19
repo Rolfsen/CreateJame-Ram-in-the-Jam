a9b6864ff830f64eb6d90917a3b30cfbd94df79f
Rolfsen <rasmus.rolfsen@mail.dk>
2017-11-19 02:37:06 +0100

    Revert "buttonscript"
    This reverts commit a23d8e2efeb7acecc591410261b49508b595ddd7.

---
+++ New Unity Project 5/Assets/ButtonPressingAnim.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPressingAnim : MonoBehaviour {
+    [SerializeField]
+    private GameObject AButton;
+    [SerializeField]
+    private GameObject BButton;
+    [SerializeField]
+    private GameObject XButton;
+    [SerializeField]
+    private GameObject YButton;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	public void SetActiveButton(string buttonName)
+    {
+        switch (buttonName)
+        {
+            case "A":
+
+                break;
+            case "B":
+
+                break;
+            case "X":
+
+                break;
+            case "Y":
+
+                break;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
