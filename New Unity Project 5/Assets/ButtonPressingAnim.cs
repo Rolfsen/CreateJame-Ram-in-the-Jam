5b8675e4932628bf8b8680e4fbcc728fbe135208
Samuel Howell <rugnirtheviking@gmail.com>
2017-11-19 03:12:21 +0100

    button script activate

--- New Unity Project 5/Assets/ButtonPressingAnim.cs
+++ New Unity Project 5/Assets/ButtonPressingAnim.cs
@@ -13,23 +13,32 @@ public class ButtonPressingAnim : MonoBehaviour {
     private GameObject YButton;
     // Use this for initialization
     void Start () {
-		
-	}
+        HideAllButtons();
+
+    }
+    void HideAllButtons()
+    {
+        AButton.SetActive(false);
+        BButton.SetActive(false);
+        XButton.SetActive(false);
+        YButton.SetActive(false);
+    }
 	public void SetActiveButton(string buttonName)
     {
+        HideAllButtons();
         switch (buttonName)
         {
             case "A":
-
+                AButton.SetActive(true);
                 break;
             case "B":
-
+                BButton.SetActive(true);
                 break;
             case "X":
-
+                XButton.SetActive(true);
                 break;
             case "Y":
-
+                YButton.SetActive(true);
                 break;
         }
     }
